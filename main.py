a4545e62874e1d51f930f59e9f0b3f5bbc0d5dae
Stephen Clements <tallcedarfarm@gmail.com>
2025-01-30 16:29:13 -0500

    Added to main.py print

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-print("main.py test 2 push")
+print("main.py test 2 push again")
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
